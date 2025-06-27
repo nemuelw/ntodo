d210498f473394e7883369b6a6249035a2435be9
Nemuel Wainaina <nemuelwainaina@proton.me>
2025-06-27 16:26:45 +0300

    Add functions to add, delete, and save todos

--- src/ntodo.nim
+++ src/ntodo.nim
@@ -1,4 +1,4 @@
-import jsony, os, strformat
+import jsony, os, sequtils, strformat
 
 type
   TodoItem = object
@@ -28,10 +28,22 @@ proc mark_todo_as_done(id: int) =
   for i in 0..<todos.len:
     if todos[i].id == id: todos[i].done = true
 
+proc edit_todo(id: int, new_title: string) =
+  for i in 0..<todos.len:
+    if todos[i].id == id: todos[i].title = new_title
+
+proc delete_todo(id: int) =
+  todos = todos.filterIt(it.id != id)
+  for i, todo in todos:
+    todos[i].id = i + 1
+
+proc save_todos() =
+  writeFile(TodoFile, toJson(todos))
+
 when isMainModule:
   echo("ntodo")
   todos = load_todos()
-  add_todo("Buy milk from the shop")
   list_todos()
-  mark_todo_as_done(1)
+  delete_todo(1)
   list_todos()
+  save_todos()
